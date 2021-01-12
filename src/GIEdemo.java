74c99d1906e9a0dcf85c99afa70fc9f59bb41ad2
whiskeyey_LvYang <lvyangcode@163.com>
2021-01-12 11:28:09 +0800

    Initial commit

---
+++ src/GIEdemo.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: lvyang
+ * @Description:
+ * @Date: 2021/1/12 11:27
+ * @Version: 1.0
+ */
+public class GIEdemo {
+    public static void main(String[] args) {
+        System.out.println("v");
+    }
+}
